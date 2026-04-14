a6b9ee3a125c98416303a6c81f5b6b69f23ae117
DmchFast <chdima28@gmail.com>
2026-04-15 00:29:05 +0300

    created table user in the db

--- book19/server.js
+++ book19/server.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 // Подключение к базе данных PostgreSQL
 const sequelize = new Sequelize('back_users_db_19', 'postgres', 'EnD37927', {
@@ -10,3 +10,17 @@ const sequelize = new Sequelize('back_users_db_19', 'postgres', 'EnD37927', {
 sequelize.authenticate()
    .then(() => console.log('Connected to PostgreSQL'))
    .catch(err => console.error('Connection error:', err));
+
+// Определение модели User
+const User = sequelize.define('User', {
+   first_name: { type: DataTypes.STRING, allowNull: false },
+   last_name: { type: DataTypes.STRING, allowNull: false },
+   age: { type: DataTypes.INTEGER, allowNull: false },
+   created_at: { type: DataTypes.DATE, defaultValue: Sequelize.NOW },
+   updated_at: { type: DataTypes.DATE, defaultValue: Sequelize.NOW }
+}, {
+   timestamps: false
+});
+
+// Синхронизация с БД
+sequelize.sync({ force: false });
