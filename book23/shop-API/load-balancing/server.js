a5eb20644c716756f31507ce454006f8d5d26970
DmchFast <chdima28@gmail.com>
2026-04-30 01:50:19 +0300

    changing the file system and server:3000 and fix server

--- book23/shop-API/load-balancing/server.js
+++ book23/shop-API/load-balancing/server.js
@@ -4,7 +4,8 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
    res.json({
-      message: 'Response from backend server',
+      //message: 'Response from backend server',
+      server: process.env.SERVER_ID || 'unknown',
       port: PORT,
       timestamp: new Date().toISOString()
    });
@@ -15,6 +16,6 @@ app.get('/health', (req, res) => {
    res.status(200).send('OK');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
    console.log(`Backend server started on port ${PORT}`); // -> http://localhost:PORT
 });
