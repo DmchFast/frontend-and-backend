c7976e809d49f878eb7b0828d3a29ff4a23765be
DmchFast <chdima28@gmail.com>
2026-05-14 10:48:18 +0300

    add book25

---
+++ book25/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { Suspense, lazy } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+// Ленивая загрузка страницы
+const About = lazy(() => import('./pages/About'));
+
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Главная</Link> | <Link to="/about">О нас</Link>
+      </nav>
+      <hr />
+      <Suspense fallback={<div className="loading">Загрузка страницы...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
---
+++ book25/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Главная страница</h1>
+      <p>C ленивой загрузкой</p>
+    </div>
+  );
+}
